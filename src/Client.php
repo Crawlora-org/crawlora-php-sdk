5c4bd2f71805a0138e045ee875895fd610fae1af
tonywangcn <tonywang.data+git@gmail.com>
2026-06-06 22:45:08 +0200

    chore: release v1.6.0-sdk.3

--- src/Client.php
+++ src/Client.php
@@ -27,7 +27,7 @@ use Crawlora\Exception\ServerError;
  */
 final class Client
 {
-    public const VERSION = '1.6.0-sdk.2';
+    public const VERSION = '1.6.0-sdk.3';
 
     public const DEFAULT_BASE_URL = 'https://api.crawlora.net/api/v1';
 
